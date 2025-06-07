d1cb313452a2e3a9c77efebe643cac106a89a9dc
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-06-07 18:07:20 -0300

    feat: add notification animation in the `AchievementButton` component

--- src/components/AchievementButton/index.tsx
+++ src/components/AchievementButton/index.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useRef } from 'react';
+import { useNotification } from '../../hooks';
 import { Icon } from '../Icon';
-import { container } from './style.css';
+import {
+  button,
+  container,
+  containerContracted,
+  containerExpanded,
+  message,
+  messageWrapper,
+} from './style.css';
 
-export function AchievementButton() {
+interface AchievementButtonProps {
+  onClick?: () => void;
+}
+
+export function AchievementButton({ onClick }: AchievementButtonProps) {
+  const { isNotification } = useNotification();
+
+  const isFirstRender = useRef<boolean>(true);
+
+  const expand = isNotification ? containerExpanded : containerContracted;
+
+  useEffect(() => {
+    if (isFirstRender.current) isFirstRender.current = false;
+  }, []);
+
   return (
-    <button type='button' className={container}>
-      <Icon name='trophy' size={36} />
-    </button>
+    <div className={`${container} ${expand}`} onClick={onClick}>
+      <button type='button' className={button}>
+        <Icon name='trophy' size={36} />
+      </button>
+
+      {!isFirstRender.current && (
+        <div className={messageWrapper}>
+          <p className={message}>Você desbloqueou um novo elemento químico!</p>
+        </div>
+      )}
+    </div>
   );
 }
--- src/components/AchievementButton/style.css.ts
+++ src/components/AchievementButton/style.css.ts
@@ -1,7 +1,54 @@
-import { style } from '@vanilla-extract/css';
+import { createVar, keyframes, style } from '@vanilla-extract/css';
 import { theme } from '../../theme.css';
 
+const messageWidth = createVar({
+  syntax: '<width>',
+  inherits: false,
+  initialValue: '64px',
+});
+
+const expandKeyframes = keyframes({
+  '0%': {
+    vars: {
+      [messageWidth]: '64px',
+    },
+  },
+  '100%': {
+    vars: {
+      [messageWidth]: '400px',
+    },
+  },
+});
+
+const contractKeyframes = keyframes({
+  '0%': {
+    vars: {
+      [messageWidth]: '400px',
+    },
+  },
+  '100%': {
+    vars: {
+      [messageWidth]: '64px',
+    },
+  },
+});
+
 export const container = style({
+  position: 'relative',
+
+  display: 'inline-block',
+
+  width: 'auto',
+  height: 'auto',
+});
+
+export const containerExpanded = style({});
+
+export const containerContracted = style({});
+
+export const button = style({
+  zIndex: 9,
+
   width: 64,
   height: 64,
 
@@ -16,18 +63,82 @@ export const container = style({
 
   borderRadius: 9999,
   border: `1px solid ${theme.color.dark['1']}`,
-
   backgroundColor: theme.color.dark['2'],
-
   color: theme.color.dark.text.secondary,
 
   userSelect: 'none',
   cursor: 'pointer',
   outline: 'none',
 
-  filter: theme.effect.boxShadow.default,
+  filter: theme.effect.dropShadow.default,
+
+  transition: 'opacity 0.3s ease-in-out',
 
   ':hover': {
     opacity: 0.9,
+  },
+});
+
+export const messageWrapper = style({
+  zIndex: 1,
+
+  position: 'absolute',
+  top: 0,
+  left: 0,
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  width: messageWidth,
+  height: 64,
+
+  padding: '8px 0px 8px 56px',
+
+  borderRadius: 9999,
+  border: `1px solid ${theme.color.dark['1']}`,
+  backgroundColor: theme.color.dark['2'],
+
+  userSelect: 'none',
+  cursor: 'pointer',
+  overflow: 'hidden',
+
+  filter: theme.effect.dropShadow.default,
+
+  vars: {
+    [messageWidth]: '64px',
+  },
+
+  selectors: {
+    [`${containerExpanded} &`]: {
+      animation: `${expandKeyframes} 0.3s ease-out forwards`,
+    },
+    [`${containerContracted} &`]: {
+      animation: `${contractKeyframes} 0.3s ease-in forwards`,
+    },
+  },
+});
+
+export const message = style({
+  fontSize: 14,
+  fontWeight: 400,
+
+  textAlign: 'right',
+
+  color: theme.color.dark.text.primary,
+
+  whiteSpace: 'nowrap',
+
+  opacity: 0,
+
+  transform: 'translateX(-10px)',
+  transition:
+    'opacity 0.2s ease-in-out 0.05s, transform 0.2s ease-in-out 0.05s',
+
+  selectors: {
+    [`${containerExpanded} &`]: {
+      opacity: 1,
+      transform: 'translateX(0)',
+    },
   },
 });
