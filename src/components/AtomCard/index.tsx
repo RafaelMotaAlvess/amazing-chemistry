842bb9e8c27bf9234624641fa70500fdd3e37293
Jonathas Meine <jonhy.meine@gmail.com>
2025-06-13 21:13:59 -0300

    feat: add context menu support to decrement selections in AtomCard

--- src/components/AtomCard/index.tsx
+++ src/components/AtomCard/index.tsx
@@ -24,12 +24,18 @@ export function AtomCard({ symbol, name, atomicNumber }: AtomCardProps) {
     );
   }, []);
 
+  const onContextMenu = useCallback((e: React.MouseEvent) => {
+      e.preventDefault();
+      setSelections(selection => (selection > 1 ? selection - 1 : 1));
+    }, []);
+
   return (
     <button
       type='button'
       title={name}
       className={container}
       onClick={onSelection}
+      onContextMenu={onContextMenu}
     >
       <div className={badgeWrapper}>
         <Badge clicks={selections} />
