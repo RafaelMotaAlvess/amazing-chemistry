90696018064e88c1ef7f35e038165e0fa5b9a422
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-06-23 20:47:38 -0300

    feat: unlock element in modal when it's unlocked

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,29 @@ import {
   Sidebar,
   WorkspaceArea,
 } from './components';
-import { useModal } from './hooks';
+import { useModal, useRecipes } from './hooks';
 import { type Molecule, recipes } from './dataset';
 import { achievementButtonWrapper, container, image } from './styles.css';
 
 function App() {
   const { onOpen } = useModal();
+  const { recipes: savedRecipes } = useRecipes();
 
-  const renderAchievements = useMemo(
-    () =>
-      recipes.map(recipe => (
+  const renderAchievements = useMemo(() => {
+    return recipes.map(recipe => {
+      const isUnlocked = !savedRecipes.find(
+        item => item.result?.formula === recipe.result.formula
+      );
+
+      return (
         <Achievement
           key={recipe.result.formula}
           molecule={recipe.result.formula as Molecule}
-          isLocked
+          isLocked={isUnlocked}
         />
-      )),
-    []
-  );
+      );
+    });
+  }, [savedRecipes]);
 
   return (
     <main className={container}>
