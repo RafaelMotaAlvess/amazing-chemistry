a75d855edb8364ecb83579f22e99c415d729c477
Jonathas Meine <jonhy.meine@gmail.com>
2025-06-27 19:52:19 -0300

    fix: adjust element key in map for turning molecule into component

--- src/components/Achievement/index.tsx
+++ src/components/Achievement/index.tsx
@@ -15,10 +15,10 @@ function moleculeToComponent(molecule: Molecule): React.JSX.Element {
     <span>
       {texts.map((text, index) => {
         return (
-          <>
-            <span key={text + (numbers[index] ?? '')}>{text}</span>
+          <span key={text + (numbers[index] ?? '')}>
+            <span>{text}</span>
             <sub>{numbers[index]}</sub>
-          </>
+          </span>
         );
       })}
     </span>
